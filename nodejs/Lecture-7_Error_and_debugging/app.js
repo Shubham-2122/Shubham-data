d78f5c267eef90b263d9fb7906c99f313185a7f2
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-02-26 14:40:22 +0530

    Error_debugging

--- nodejs/Lecture-7_Error_and_debugging/app.js
+++ nodejs/Lecture-7_Error_and_debugging/app.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const testingSyntax = require('./synatx')
+const runtime = require('./runtime')
+const logical = require("./logical");
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method);
-    testingSyntax();
+    // testingSyntax();
+    // runtime();
+    logical();
 })
 
-const PORT = 3000;
+const PORT = 3001;
 
 server.listen(PORT,()=>{
     console.log(`Server runing on address http://localhost:${PORT}`);
---
+++ nodejs/Lecture-7_Error_and_debugging/runtime.js
@@ -0,0 +1,10 @@
+const runtime = ()=>{
+
+    // not Define as runtime
+    // console.log(x)
+
+    // this is not Function
+    // let num = 98;
+    // num() 
+};
+module.exports =runtime;
