91dd9cde597186c4c6c08cf22a5b78827fe9e2de
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-03-09 18:39:31 +0530

    express

--- nodejs/Lecture-8_Express/app.js
+++ nodejs/Lecture-8_Express/app.js
@@ -1,16 +1,37 @@
-// core module
-const http = require('http');
+// // core module
+// const http = require('http');
 
 // global module
 const express = require('express');
 
 // local module user create
-const requestHanler = require('./user');
+// const requestHanler = require('./user');
 
-const server = http.createServer(requestHanler)
+ const app = express();
+
+app.get("/",(req,res,next)=>{
+    console.log("Came in first middleware",req.url , req.method)
+    //  res.send("<h1>hello welcome first middleware </h2>")
+    next();
+ })
+
+ app.post("/submit-details",(req,res,next)=>{
+    console.log("Came in second middleware",req.url , req.method)
+    res.send("<h1>hello this Express js</h2>")
+ })
+
+ app.use("/",(req,res,next)=>{
+    console.log("Came in another middleware",req.url , req.method)
+     res.send("<h1>hello another first middleware </h2>")
+    // next();
+ })
+
+
+
+// const server = http.createServer(app)
 
 const PORT = 3001;
 
-server.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Server Runing on Address http://localhost:${PORT}`);
 });
